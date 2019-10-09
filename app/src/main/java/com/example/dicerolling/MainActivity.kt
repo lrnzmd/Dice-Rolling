f591c9f5f0c257c1c4fe5b4ec4f398d6c0a5592a
lrnzmd <55979889+lrnzmd@users.noreply.github.com>
2019-10-09 11:51:59 +0200

    toast

---
+++ app/src/main/java/com/example/dicerolling/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.dicerolling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        }
+
+    }
+
+
+
+
