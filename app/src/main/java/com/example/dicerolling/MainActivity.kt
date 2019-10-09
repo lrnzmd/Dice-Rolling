2d0b558bac35148c9649a10c21b50088fee72344
lrnzmd <55979889+lrnzmd@users.noreply.github.com>
2019-10-09 12:43:35 +0200

    collegate le immagini

--- app/src/main/java/com/example/dicerolling/MainActivity.kt
+++ app/src/main/java/com/example/dicerolling/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.dicerolling
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -21,11 +23,20 @@ class MainActivity : AppCompatActivity() {
         }
 
     private fun rollDice() {
-        val resultText: TextView= findViewById(R.id.result_text)
 
         val randomInt = Random().nextInt(6)+1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-            resultText.text = randomInt.toString()
+           val diceImage: ImageView = findViewById(R.id.dice_immage)
+        diceImage.setImageResource(drawableResource)
+
     }
 
 }
