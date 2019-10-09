1c2ba24ac65f518e88303d6274ed3ab65b857757
lrnzmd <55979889+lrnzmd@users.noreply.github.com>
2019-10-09 13:52:27 +0200

    fine

--- app/src/main/java/com/example/dicerolling/MainActivity.kt
+++ app/src/main/java/com/example/dicerolling/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        
+
         diceImage.setImageResource(drawableResource)
 
     }
